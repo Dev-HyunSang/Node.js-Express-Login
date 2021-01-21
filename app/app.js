fe1cdaeab2793f1e444d054f508725cd548abe78
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-01-21 18:14:51 +0900

    [UPDATE🛠] Login API in Server

--- app/app.js
+++ app/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
-
+const bodyparser = require('body-parser');
 require('dotenv').config();
+
 const home = require('./src/routes/home');
 const app = express();
 
 app.set('views', "./src/views");
 app.set('view engine', 'ejs');
 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/src/public`));
 app.use("/", home);
 app.use('/login', home);
