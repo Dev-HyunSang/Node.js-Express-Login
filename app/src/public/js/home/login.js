423ec169e093faca4c4d5a6fa69ccc8b2d79a0c6
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-01-21 17:50:22 +0900

    [UPDATE🛠] 로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -10,4 +10,12 @@ function login() {
         password: psword.value,
     };
     console.log(req);
+    console.log(JSON.stringify(req));
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify(req)
+    });
 } 
